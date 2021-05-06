1b780cde489436b8dfca970ea1f65e4a70539af3
p40420jc <james.coffey@student.manchester.ac.uk>
2021-05-06 01:09:51 +0100

    Nodes are now uniquely identified by their label

--- src/main/java/automata/core/Node.java
+++ src/main/java/automata/core/Node.java
@@ -6,29 +6,34 @@ import java.util.List;
 public class Node {
     static private int nodeCount;
 
-    private final int ID;
+    /**
+     * The label should be unique in a graph
+     */
+    private String label;
 
-    public String label;
     List<Node> connections = new ArrayList<>();
+
     private boolean isAccepting;
 
+    protected Node() {
+        nodeCount++;
+    }
+
     /**
-     * Initialises a newly created node with a unique <code>ID</code> and a <code>label</code> set by <code>label_</code>
+     * Initialises a newly created node with a unique <code>label</code>
      *
      * @param label_ What this node should be labelled as
      */
     public Node(String label_) {
         label = label_;
-        ID = nodeCount;
         nodeCount++;
     }
 
     /**
-     * Initialises a newly created node with a unique <code>ID</code> and a <code>label</code> set by <code>label_</code>.
-     * Also sets the <code>isAccepting</code> field to the <code>isAccepting_</code> parameter
+     * Initialises a newly created node with a unique <code>label</code>
      *
      * @param label_       What this node should be labeled as
-     * @param isAccepting_ Weather this node is accepting or not
+     * @param isAccepting_ Whether this node is accepting or not
      */
     public Node(String label_, boolean isAccepting_) {
         this(label_);
@@ -44,30 +49,39 @@ public class Node {
     }
 
     /**
-     * @param accepting Sets weather the node is accepting
+     * @param accepting Sets whether the node is accepting
      */
     void setAccepting(boolean accepting) {
         isAccepting = accepting;
     }
 
     /**
-     * @return Gets the <code>ID</code> of the node
+     * @return Gets the label of the node
      */
-    public int getID() {
-        return ID;
+    public String getLabel() {
+        return label;
     }
 
     /**
      * Overrides the toString method in <code>java.lang.Object</code>
      *
-     * @return String representation of the node, including its label, ID and list of connections
+     * @return String representation of the node, including its label and list of connections
      */
     public String toString() {
+        // Convert the connections ArrayList to a String[] of the labels of the connections
         String[] connections_string_array = new String[connections.size()];
-        for (int i = 0; i < connections.size(); i++) {
-            connections_string_array[i] = connections.get(i).label;
-        }
+        for (int i = 0; i < connections.size(); i++) connections_string_array[i] = connections.get(i).getLabel();
 
-        return "Label: " + label + ", Id: " + ID + ", Connections: " + String.join(",", connections_string_array);
+        return "Label: " + getLabel() + ", Connections: " + String.join(",", connections_string_array);
     }
+
+
+    public boolean equals(Node node) {
+        return getLabel().equals(node.getLabel());
+    }
+
+    public boolean equals(String label) {
+        return getLabel().equals(label);
+    }
+
 }
