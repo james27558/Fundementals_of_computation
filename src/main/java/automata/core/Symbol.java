faebf2c4c05bf2e9e90c19afa2227aed45ba8271
p40420jc <james.coffey@student.manchester.ac.uk>
2021-05-07 16:12:31 +0100

    Added Symbol and Transition

---
+++ src/main/java/automata/core/Symbol.java
@@ -0,0 +1,29 @@
+package automata.core;
+
+import java.util.Objects;
+
+public class Symbol {
+    public static final Symbol EPSILON = fromString("");
+    private String stringRepresentation;
+    private boolean isEpsilon;
+
+    private Symbol() {
+    }
+
+    static Symbol fromString(String s) {
+        Symbol symbol = new Symbol();
+        symbol.stringRepresentation = s;
+        symbol.isEpsilon = s.equals("");
+
+        return symbol;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Symbol symbol = (Symbol) o;
+        return Objects.equals(stringRepresentation, symbol.stringRepresentation);
+    }
+
+}
---
+++ src/main/java/automata/core/Transition.java
@@ -0,0 +1,68 @@
+package automata.core;
+
+public class Transition {
+    private Node source;
+    private Node destination;
+
+    private Symbol symbol;
+
+    /**
+     * Constructs a transition between nodes.
+     *
+     * @param source      Source node
+     * @param destination Destination node
+     * @param symbol      Symbol to transition over
+     */
+    public Transition(Node source, Node destination, Symbol symbol) {
+        this.source = source;
+        this.destination = destination;
+        this.symbol = symbol;
+    }
+
+    /**
+     * Constructs a transition between nodes.
+     *
+     * @param source      Source node
+     * @param destination Destination node
+     * @param symbol      Symbol to transition over
+     */
+    public Transition(Node source, Node destination, String symbol) {
+        this(source, destination, Symbol.fromString(symbol));
+    }
+
+    /**
+     * Given this transition between two nodes, return whether a given symbol would cause it
+     *
+     * @param testSymbol Symbol to test
+     * @return Whether <code>testSymbol</code> would cause this transition to activate
+     */
+    public boolean willSymbolCauseTransition(Symbol testSymbol) {
+        return symbol.equals(testSymbol);
+    }
+
+    /**
+     * Given this transition between two nodes, return whether a given symbol would cause it
+     *
+     * @param testSymbol Symbol to test
+     * @return Whether <code>testSymbol</code> would cause this transition to activate
+     */
+    public boolean willSymbolCauseTransition(String testSymbol) {
+        return Symbol.fromString(testSymbol).equals(getSymbol());
+    }
+
+    public Node getSource() {
+        return source;
+    }
+
+    public Node getDestination() {
+        return destination;
+    }
+
+    public Symbol getSymbol() {
+        return symbol;
+    }
+
+    public String toString() {
+        return "Source: " + getSource() + ", Destination: " + getDestination() + ", Symbol: '" + getSymbol() + "'";
+    }
+}
